8480faf710b5a0de1f715826588a5f55ccc63bf5
Shivani Pandey <104144826+shivanipandey22@users.noreply.github.com>
2022-09-29 23:59:59 +0530

    LandingPageStarted

--- gustoclone/src/App.js
+++ gustoclone/src/App.js
@@ -1,11 +1,14 @@
 // import logo from './logo.svg';
 import './App.css';
+import LandingPage from './Components/LandingPage/LandingPage';
 import Navbar from './Components/Navbar/Navbar';
+
 
 function App() {
   return (
     <div className="App">
  <Navbar/>
+ <LandingPage/>
     </div>
   );
 }
---
+++ gustoclone/src/Components/LandingPage/LandingPage.jsx
@@ -0,0 +1,144 @@
+import React from 'react'
+
+import "./LandingPage.css"
+
+const LandingPage = () => {
+  return (
+    <div id='landing_page'>
+        <div id='left1'>
+    <h1 >One place for payroll,benefits, and so much more.</h1>
+    <p>People are at the heart of every successful business. Gusto puts the tools you need to hire, pay, insure, and support your team in one platform.</p>
+    </div>
+    <div>
+        <button >Create account</button>
+        <button>Learn how Gusto works</button>
+    </div>
+     <div>
+        <h1>Quickly pay your employess and contractors.</h1>
+        <button>Learn how payroll works</button>
+        <img src="https://prod.gusto-assets.com/wp-content/uploads/Insights-and-reporting%402x.png" alt="" />
+        <div>  
+            <h3>Full-service payroll 
+            <p>With Gusto, payroll takes just a few clicks. We automatically file your taxes, help with compliance, and can identify hidden tax credits to save you money. In many cases we can even transfer your payroll and benefits data for you.
+                <div>
+                    <p>Unlimited payrolls runs</p>
+                    <p>State tax registration</p>
+                    <p>International contractor payments</p>
+                    <p>Automatic W-2s and 1099s</p>
+                </div>
+            </p>
+        </h3>
+        
+        </div>
+
+
+        <div>
+            <h3>Time and attendance
+                <p>Gusto automatically calculates and syncs your team's hours, PTO, and holidays with payroll. Plus, we help you stay compliant with alerts, software integrations, and more.
+                   
+                    <div>
+                         <p>Time off requests and approvals</p>
+                    <p>Project tracking and cost reports </p>
+                    <p>PTO policies and reporting</p>
+                    <p>Mobile time tracking</p>
+                    </div>
+                </p>
+            </h3>
+            
+        </div>
+
+        <div>
+            <h3>Insights and reporting
+                <p>
+                    No more guessing games. Use competitive compensation tools, employee Our competitive compensation tools, employee feedback surveys, and workforce costing features make tough business decisions a lot easier.
+                    <div>
+                        <p>R&D tax credits</p>
+                        <p>Workforce and project costing</p>
+                        <p>Actionable</p>
+                        <p>Automatic custom reports</p>
+                    </div>
+                </p>
+            </h3>
+        </div>
+
+     </div>
+
+     <div>
+        <h1>Pricing plans start at just $46/month</h1>
+        <p>No matter the size or stage of your business we'll work with you to find the plan that best suits your needs. </p>
+        <button>See pricing</button>
+     </div>
+
+     <div>
+        <img src="https://prod.gusto-assets.com/wp-content/uploads/Talent-management%402x.png" alt="" />
+        <h1>
+            Build a better workplace with flexible benefits and HR tools.
+        </h1>
+        <button>Learn how benefits work</button>
+
+        <div>
+            <h3>Employee benefit
+                <p>We provide a wide range of health and financial benefits to keep your team happy, productive, and engaged.
+                    <div>
+                        <p>Medical, dental, life, and vision</p>
+                        <p>Trusted benefits advisors</p>
+                        <p>HSA and FSA</p>
+                        <p>401(k), 529, workers' comp</p>
+                    </div>
+                </p>
+            </h3>
+        </div>
+
+        <div>
+            <h3>Hiring and onboarding
+                <p>From hiring your first employees to scaling your growing business, Gusto helps you find and support great people from day 1 to 1000.</p>
+                <div>
+                    <p>Job posts and applicant tracking</p>
+                    <p>Custom onboarding checklists</p>
+                    <p>Offer letter templates</p>
+                    <p>Integrations and app provisioning</p>
+                </div>
+            </h3>
+        </div>
+
+        <div>
+            <h3>Talent management
+                <p>Whether you're in an office, remote, or in between, give your employees a clear path for growth with performance reviews, trainings, and career development tools.
+                    <div>
+                        <p>Automated feedback requests</p>
+                        <p>Learning and development tools</p>
+                        <p>Performance review templates</p>
+                        <p>Review progress tracking</p>
+                    </div>
+                </p>
+            </h3>
+        </div>
+     </div>
+
+     <div>
+        <h1>
+            "I can't imagine running out business without Gusto. We use it for payroll, compliance, 401(k), employee benefits, the HR resourcr center...the list goes on."
+        </h1>
+    <img src="https://prod.gusto-assets.com/wp-content/uploads/signature-Sara-Jane-Hess-1.svg" alt="" />   Operations Director, SPI Media LLC
+
+     </div>
+
+     <div>
+        <h4>Prices start at just $46/month</h4>
+        <h1>Get your team working with Gusto.</h1>
+        <h2>Create an account</h2>
+        <p>It's free to start. Tell us about business and we'll find the best plan for your needs.</p>
+
+        <h2>Add your people</h2>
+        <p>Enter your employees into Gusto. They can even self-onboard to save you time.</p>
+
+        <h2>Run your first payroll</h2>
+        <p>Once we have you employee and tax info, you can run payroll in just a few clicks.</p>
+
+        <button>Create account</button>
+     </div>
+    </div>
+  )
+}
+
+export default LandingPage;
