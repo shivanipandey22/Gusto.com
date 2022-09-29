c46c2db0d8094113752313bbb469b7b7a5cef844
Shivani Pandey <104144826+shivanipandey22@users.noreply.github.com>
2022-09-29 23:42:07 +0530

    Navbar

---
+++ gustoclone/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+
+    <div id='container'>
+      
+        <img src='https://bit.ly/3dQ1PQ1'/>
+        <div id='nav_box'>
+            <label >Why Gusto</label>
+            <label >Payroll and more</label>
+            <label >Pricing</label>
+            <label >For Accountants</label>
+            <label >Resources</label>
+            <label >Built for you</label>
+
+        </div>
+        <button>Click</button>
+    </div>
+  )
+}
+
+export default Navbar;
